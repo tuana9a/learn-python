211a8c301e98353ce1dbe005a8c4302595cf9f27
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-06-23 06:32:04 +0700

    class prop but also default value for its instance

--- basic/oop.py
+++ basic/oop.py
@@ -1,10 +1,12 @@
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-  def myfunc(self):
-    print("Hello my name is " + self.name + " and I'm " + str(self.age) + " years old")
+    def myfunc(self):
+        print("Hello my name is " + self.name +
+              " and I'm " + str(self.age) + " years old")
+
 
 p1 = Person("John", 36)
 p1.myfunc()
@@ -14,13 +16,16 @@ The self parameter is a reference to the current instance of the class, and is u
 
 It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
 """
-class Person:
-  def __init__(mysillyobject, name, age):
-    mysillyobject.name = name
-    mysillyobject.age = age
 
-  def myfunc(abc):
-    print("Hello my name is " + abc.name)
+
+class Person:
+    def __init__(mysillyobject, name, age):
+        mysillyobject.name = name
+        mysillyobject.age = age
+
+    def myfunc(abc):
+        print("Hello my name is " + abc.name)
+
 
 p1 = Person("John", 36)
 p1.myfunc()
@@ -30,38 +35,68 @@ del p1
 # p1
 
 # kế thừa class Person
+
+
 class Student(Person):
-  def __init__(mysillyobject, name, age):
-    super().__init__(name, age)
-  pass
+    def __init__(mysillyobject, name, age):
+        super().__init__(name, age)
+    pass
+
 
 s = Student("tuana9a", 12)
 s.myfunc()
 
+
 class Duck:
-    sound='quackk'
-    walking='walk like a duck'
+    sound = 'quackk'
+    walking = 'walk like a duck'
+
     def hello(self):
         print(self.sound)
+
+
 d = Duck()
 d.hello()
 
-class Test1():
-  def __init__(self, name):
-    super().__init__()
-    self.test1 = f'hi im {name}'
-    
-  @property
-  def name(self):
-    return f'my test1 is {self.test1}'
-    
-  @classmethod
-  def name1(cls):
-    return f'"co con cac day la class "' + str(cls)
+
+class Person():
+    def __init__(self, name):
+        # super().__init__()
+        self.name = f'hi im {name}'
+
+    @property
+    def nice_name(self):
+        return f'my nice name is {self.name}'
+
+    @classmethod
+    def get_class_name(cls):
+        return f'"co chac day la class "' + str(cls)
 
 
 # print(Test1("tuan").name)
-print(Test1("tuan").name1)
-print(Test1("tuan").name1())
-print(Test1.name1)
-print(Test1.name1())
+print(Person("tuan").get_class_name)
+print(Person("tuan").get_class_name())
+print(Person.get_class_name)
+print(Person.get_class_name())
+
+class Test1:
+    username: str = "tuan"
+    password: str = "deptrai"
+
+print(Test1.username)
+
+t = Test1()
+
+print(t.username)
+
+t.username = "long"
+
+print(t.username, Test1.username)
+
+Test1.username = "the fuck"
+
+print(t.username, Test1.username)
+
+t2 = Test1()
+
+print(Test1.username, t2.username)
